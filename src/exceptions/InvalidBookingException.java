bc955506edbecbd518b273469f2b2b7dd64a020c
GOURAV <gouravpatel20032003@gmail.com>
2025-05-14 13:00:01 +0530

    2nd commit

--- src/exceptions/InvalidBookingException.java
+++ src/exceptions/InvalidBookingException.java
@@ -1,5 +1,11 @@
 package exceptions;
 
-public class InvalidBookingException {
-
+public class InvalidBookingException extends Exception{
+	
+	static String message = "The booking is invalid";
+	
+	public InvalidBookingException(){
+		System.out.println(message);
+	}
+	
 }
